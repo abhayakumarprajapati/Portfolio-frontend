9e13fd65bdf8c3d86f33caca14e6d47cb018796a
abhayakumarprajapati <abhayprajapati1000@gmail.com>
2025-02-22 20:39:51 +0530

    added background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
+import Navbar from "./Navbar/page";
 
 
 export default function Home() {
+  console.log("render page")
+
   return (
-    <div style={{height:"100vh",display:"flex",justifyContent:"center",alignItems:"center"}}>
-      <h3><b style={{fontSize:"70px"}}>H</b>ello i am abhay</h3>
-    </div>
+    <>
+      <div style={{ backgroundColor: "gray" }}>
+        <Navbar />
+        <div style={{ height: "100vh", display: "flex", justifyContent: "center", alignItems: "center" }}>
+          <h3><b style={{ fontSize: "70px" }}>H</b>ello i am abhay</h3>
+        </div>
+      </div>
+    </>
   );
 }
