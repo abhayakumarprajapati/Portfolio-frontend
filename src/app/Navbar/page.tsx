5cd1c0232ec449adb14b4b8f2e3cf8dfd08ab73f
abhayakumarprajapati <abhayprajapati1000@gmail.com>
2025-02-22 22:50:14 +0530

    styles added

--- src/app/Navbar/page.tsx
+++ src/app/Navbar/page.tsx
@@ -1,28 +1,30 @@
 "use client"
 
 
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './styles.module.css'
 
 
 const Navbar = () => {
 
+    const [isOpen, setIsOpen] = useState(false);
+
 
     const toggleMenu = () => {
-
+        setIsOpen(!isOpen);
     }
 
 
     return (
         <nav className={styles.navbar}>
             <div className={styles.logo}>Abhay Prajapati</div>
-            <ul className={styles.navlinks}>
+            <ul className={`${styles.navlinks} ${isOpen ? styles.active : ""}`} >
                 <li><a href="#">Home</a></li>
                 <li><a href="#">About</a></li>
                 <li><a href="#">Services</a></li>
                 <li><a href="#">Contact</a></li>
             </ul>
-            <div className={styles.hamburger} onClick={() => toggleMenu()}>☰</div>
+            <div className={styles.hamburger} onClick={toggleMenu}>☰</div>
         </nav>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Navbar from "./Navbar/page";
+import styles from './styles.module.css'
 
 
 export default function Home() {
@@ -6,10 +7,19 @@ export default function Home() {
 
   return (
     <>
-      <div style={{ backgroundColor: "gray" }}>
+      <div style={{ background: "linear-gradient(90deg, rgba(1,14,35,1) 49%, rgba(67,61,66,1) 100%, rgba(15,15,150,1) 100%)" }}>
         <Navbar />
-        <div style={{ height: "100vh", display: "flex", justifyContent: "center", alignItems: "center" }}>
-          <h3><b style={{ fontSize: "70px" }}>H</b>ello i am abhay</h3>
+        <div className={styles.mainContainer}>
+          <div className={styles.textContainer}>
+            <p className={`${styles.textStyle} ${styles.tomato}`}>HELLO!</p>
+            <h1 className={`${styles.white} ${styles.textStyle}`}>I Am Abhay Prajapati</h1>
+            <p className={`${styles.white} ${styles.textStyle}`}>
+              I am a Full stack developer with more than 2 years of experience. My expertise is to create and design websites.
+            </p>
+          </div>
+          <div className={styles.imageContainer}>
+            Image
+          </div>
         </div>
       </div>
     </>
