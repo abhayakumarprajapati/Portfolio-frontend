f81bb3c823397254aa013fead42fbbd07f9bd3b3
abhayakumarprajapati <abhayprajapati1000@gmail.com>
2025-02-22 20:52:27 +0530

    create navbar

--- src/app/Navbar/page.tsx
+++ src/app/Navbar/page.tsx
@@ -1,10 +1,29 @@
+"use client"
+
+
 import React from 'react'
+import styles from './styles.module.css'
+
 
 const Navbar = () => {
+
+
+    const toggleMenu = () => {
+
+    }
+
+
     return (
-        <div>
-            Navbar
-        </div>
+        <nav className={styles.navbar}>
+            <div className={styles.logo}>Abhay Prajapati</div>
+            <ul className={styles.navlinks}>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+            <div className={styles.hamburger} onClick={() => toggleMenu()}>☰</div>
+        </nav>
     )
 }
 
